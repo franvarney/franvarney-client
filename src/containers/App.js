19567e7aaf7aa6645172cc5a310d39f56603cb6a
Fran Varney <fran@modulus.io>
2016-01-15 08:36:54 -0500

    Pass down all children instead of rendering the home page component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,10 @@
 import React, {createClass} from 'react'
 import {RouteHandler} from 'react-router'
 
-import Home from '../pages/Home'
-
 let App = createClass({
   render() {
     return (
-      <div><Home /></div>
+      <div>{this.props.children}</div>
     )
   }
 })
