82b7441e9f3efab7fb7f87318834a8d4d0c51db5
Fran Varney <fran@modulus.io>
2016-01-16 16:28:13 -0500

    Add blog pages to routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,21 @@
-import createBrowserHistory from 'history/lib/createBrowserHistory'
+import {createHistory} from 'history'
 import React from 'react'
 import {render} from 'react-dom'
-import {Route, Router} from 'react-router'
+import {IndexRoute, Route, Router} from 'react-router'
 
 import App from './containers/App'
+import Blogs from './containers/Blog/All'
+import BlogShow from './containers/Blog/Show'
 import CSS from './styles/index.styl'
+import Home from './containers/Home'
 
 let routes = (
-  <Router history={createBrowserHistory()}>
-    <Route path='/' component={App} />
+  <Router history={createHistory()}>
+    <Route path='/' component={App}>
+      <IndexRoute component={Home} />
+      <Route path="/blog" component={Blogs} />
+      <Route path="/blog/:slug" component={BlogShow} />
+    </Route>
   </Router>
 )
 
