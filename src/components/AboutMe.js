6e2e832487f28492b3d7dcd03aa1d306597bd0db
Fran Varney <fran@modulus.io>
2016-01-19 09:10:05 -0500

    Add about me component

---
+++ src/components/AboutMe.js
@@ -0,0 +1,15 @@
+import React, {createClass, PropTypes} from 'react'
+
+let AboutMe = createClass({
+  render() {
+    return (
+      <div className="about-me-container container">
+        <h2>About Me</h2>
+        <p>I'm Fran Varney. A software developer, a video gamer, mechanical
+        keyboard enthusiast, dog lover, and long time photographer.</p>
+      </div>
+    )
+  }
+})
+
+export default AboutMe
