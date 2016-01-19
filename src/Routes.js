94fbdf2bfbbc8a54a7afe3007a0e73fcab33fdea
Fran Varney <fran@modulus.io>
2016-01-19 09:17:47 -0500

    Add blog routes and change router history to hash history

--- src/Routes.js
+++ src/Routes.js
@@ -1,7 +1,7 @@
-import {createHistory} from 'history'
+import {createHashHistory} from 'history'
 import React from 'react'
 import {render} from 'react-dom'
-import {IndexRoute, Route, Router} from 'react-router'
+import {IndexRoute, Route, Router, useRouterHistory} from 'react-router'
 
 import App from './containers/App'
 import Blogs from './containers/Blog/All'
@@ -9,12 +9,14 @@ import BlogShow from './containers/Blog/Show'
 import CSS from './styles/index.styl'
 import Home from './containers/Home'
 
+const appHistory = useRouterHistory(createHashHistory)({ queryKey: false })
+
 let routes = (
-  <Router history={createHistory()}>
+  <Router history={appHistory}>
     <Route path='/' component={App}>
       <IndexRoute component={Home} />
-      <Route path="/blog" component={Blogs} />
-      <Route path="/blog/:slug" component={BlogShow} />
+      <Route path="blog" component={Blogs} />
+      <Route path="blog/:slug" component={BlogShow} />
     </Route>
   </Router>
 )
