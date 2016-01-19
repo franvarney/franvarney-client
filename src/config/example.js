69d7bff360f090e8fa66fc31c6f8c1abb53da93a
Fran Varney <fran@modulus.io>
2016-01-19 09:03:14 -0500

    Add social media urls

--- src/config/example.js
+++ src/config/example.js
@@ -2,5 +2,11 @@ export default {
   api: {
     url: 'http://api.url.com',
     token: 'someToken'
+  },
+  social: {
+    facebook: 'http://www.facebook.com/profile',
+    github: 'http://www.github.com/profile',
+    linkedin: 'http://www.linkedin.com/profile',
+    twitter: 'http://www.twitter.com/profile'
   }
 }
