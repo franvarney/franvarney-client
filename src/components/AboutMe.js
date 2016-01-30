bfd59107f251be3dad12ff02619216babaec5e79
Fran Varney <fran@modulus.io>
2016-01-30 17:55:11 -0500

    Change about me description

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -5,8 +5,12 @@ let AboutMe = createClass({
     return (
       <div className="about-me-container container">
         <h2>About Me</h2>
-        <p>I'm Fran Varney. A software developer, a video gamer, mechanical
-        keyboard enthusiast, dog lover, and long time photographer.</p>
+        <p>Hi! I'm Fran Varney. I'm currently a software engineer at Modulus.</p>
+        <p>I grew up loving art and started on a path to photography, but after
+        taking a programming class for fun, I ended up loving it and made my move
+        into development.</p>
+        <p>In my free time I still love photography, along with drooling over
+        mechanical keyboards and playing video games.</p>
       </div>
     )
   }
