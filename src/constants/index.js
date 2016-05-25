cf513f9d6157350b010043b39cb89ffa058ab86e
Fran Varney <fvarney@progress.com>
2016-05-25 19:13:39 -0400

    Move day and month names to constants file

---
+++ src/constants/index.js
@@ -0,0 +1,24 @@
+export default {
+  monthNames: [
+    'January',
+    'February',
+    'March',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ],
+  dayNames: [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+  ]
+}
