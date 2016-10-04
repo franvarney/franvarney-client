71ba50eb7eac70f13bbc66767a57bc94ba570eb3
Fran Varney <fran.varn@gmail.com>
2016-10-04 16:21:23 -0400

    Pass undefined post instead of object

--- src/containers/Blog/Show.js
+++ src/containers/Blog/Show.js
@@ -5,32 +5,29 @@ import BlogPost from '../../components/Blog/Post'
 import Config from '../../config'
 import DefaultLayout from '../../layouts/Default'
 
-let BlogShow = createClass({
-  getInitialState() {
+const BlogShow = createClass({
+  getInitialState () {
     return {
-      post: {},
+      post: undefined,
       slug: this.props.params.slug
     }
   },
 
-  getPost() {
+  getPost () {
     Request
       .get(`${Config.api.url}/posts/${this.state.slug}`)
       .set('Content-Type', 'application/json')
       .end((err, res) => {
         if (err) console.log(err)
-
-        this.setState({
-          post: res.body
-        })
+        this.setState({ post: res.body })
       })
   },
 
-  componentDidMount() {
+  componentDidMount () {
     this.getPost()
   },
 
-  render() {
+  render () {
     return (
       <DefaultLayout classes="page-blog-show">
         <BlogPost post={this.state.post} hasComments={true} />
@@ -38,5 +35,7 @@ let BlogShow = createClass({
     )
   }
 })
+
+BlogShow.displayName = 'BlogShowContainer'
 
 export default BlogShow
