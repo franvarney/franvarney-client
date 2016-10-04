d5ed1ee2ff47891d732574bedc6aeaf6bfb65cce
Fran Varney <fran.varn@gmail.com>
2016-10-04 16:18:31 -0400

    Update classes

--- src/components/Layout/LinkButton.js
+++ src/components/Layout/LinkButton.js
@@ -1,16 +1,29 @@
 import React, {createClass, PropTypes} from 'react'
 import {Link} from 'react-router'
 
-let LinkButton = createClass({
-  render() {
+const LinkButton = createClass({
+  getDefaultProps () {
+    return {
+      text: 'Submit'
+    }
+  },
+
+  render () {
     const {path, text} = this.props
 
     return (
-      <span className="link-button">
+      <span className="link">
         <Link to={path}>{text} &raquo;</Link>
       </span>
     )
   }
 })
+
+LinkButton.displayName = 'LinkButtonComponent'
+
+LinkButton.propTypes = {
+  path: PropTypes.string.isRequired,
+  text: PropTypes.string
+}
 
 export default LinkButton
