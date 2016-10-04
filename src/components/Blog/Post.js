7e69abc920eca16ac694722e1889b773301b4d1c
Fran Varney <fran.varn@gmail.com>
2016-10-04 16:22:57 -0400

    Update to work with blog creation preview and add new properties

--- src/components/Blog/Post.js
+++ src/components/Blog/Post.js
@@ -1,4 +1,4 @@
-import React, {createClass} from 'react'
+import React, {createClass, PropTypes} from 'react'
 import ReactDisqusThread from 'react-disqus-thread'
 import ReactMarkdown from 'react-markdown'
 import {Link} from 'react-router'
@@ -6,33 +6,68 @@ import {Link} from 'react-router'
 import FormatDate from '../../utils/format-date'
 
 const BlogPost = createClass({
-  getInitialState () {
+  getDefaultProps() {
     return {
+      editing: false,
+      hasComments: false,
       post: {}
     }
   },
 
-  render () {
-    let {content, createdAt, _id, slug, title} = this.props.post
-    let header = (<h2>{title}</h2>), comments = null
+  shouldComponentUpdate (nextProps) {
+    return (nextProps.post !== this.props.post) || // for showing post
+           (this.props.editing && nextProps.post === this.props.post) // for creating post
+  },
 
-    if (!this.props.hasComments) header = (<Link to={`/blog/${slug}`}><h2>{title}</h2></Link>)
-    if (this.props.hasComments) comments = (<ReactDisqusThread
-                                  shortname="franvarney"
-                                  identifier={_id}
-                                  title={title} />)
+  render () {
+    const {editing, hasComments, post} = this.props
+    const {
+      category, content, createdAt, _id,
+      isHtml=false, slug, tags=[], title
+    } = post
 
     return (
-      <div className="blog-post-container container">
-        {header}
-        <p className="date"><em>Posted on {FormatDate(createdAt)}</em></p>
-        <ReactMarkdown source={content || 'Loading...' } />
-        {comments}
+      <div className="blog-post row">
+        {(hasComments || editing) && <h2>[{category}] {title}</h2>}
+        {(!hasComments && !editing) &&
+          <Link to={`/blog/${slug}`}>
+            <h2>[{category}] {title}</h2>
+          </Link>
+        }
+        {createdAt &&
+          <p className="date">
+            <em>Posted on {FormatDate(createdAt)}</em>
+          </p>
+        }
+        {!post.isHtml && <ReactMarkdown source={content} />}
+        {post.isHtml && <div dangerouslySetInnerHTML={{ __html: content }}></div>}
+        {tags.length > 0 && <p>Tags: {tags.join(', ')}</p>}
+        {(hasComments && !editing) &&
+          <ReactDisqusThread
+            shortname="franvarney"
+            identifier={_id}
+            title={title} />
+        }
       </div>
     )
   }
 })
 
 BlogPost.displayName = 'BlogPostComponent'
+
+BlogPost.propTypes = {
+  editing: PropTypes.bool,
+  hasComments: PropTypes.bool,
+  post: PropTypes.shape({
+    _id: PropTypes.string,
+    category: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired,
+    createdAt: PropTypes.string,
+    isHtml: PropTypes.bool,
+    slug: PropTypes.string,
+    tags: PropTypes.array.isRequired,
+    title: PropTypes.string.isRequired
+  })
+}
 
 export default BlogPost
