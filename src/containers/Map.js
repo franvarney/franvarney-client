cdff3afe6ea0e418453bb8db5769eb17437c2002
Fran Varney <fran.varn@gmail.com>
2016-10-13 14:48:00 -0400

    Add condensed query parameter, use SDK method, and clean up code

--- src/containers/Map.js
+++ src/containers/Map.js
@@ -2,28 +2,35 @@ import GoogleMapReact from 'google-map-react'
 import React, {createClass} from 'react'
 import Request from 'superagent'
 
+import Button from '../components/Layout/Button'
 import Config from '../config'
 import DefaultLayout from '../layouts/Default'
 import Marker from '../components/Map/Marker'
 import MarkerDetails from '../components/Map/MarkerDetails'
+import {Place} from '../utils/sdk'
 import Stylers from '../components/Map/stylers'
 
 const GoogleMap = createClass({
-  getDefaultProps() {
+  getDefaultProps () {
     return {
       center: { lat: 39.1031182, lng: -84.5120196 }, // Cincinnati
       zoom: 4
     }
   },
 
-  getInitialState() {
+  getInitialState () {
     return {
-      allPlaces: [],
-      places: []
+      error: null,
+      places: {
+        all: [],
+        current: [],
+        self: [],
+        visitors: []
+      }
     }
   },
 
-  createMapOptions(maps) {
+  createMapOptions (maps) {
     return {
       panControl: true,
       mapTypeControl: false,
@@ -32,22 +39,8 @@ const GoogleMap = createClass({
     }
   },
 
-  getLocations() {
-    Request
-      .get(`${Config.api.url}/places`)
-      .set('Content-Type', 'application/json')
-      .end((err, response) => {
-        if (err) console.error(err)
-
-        this.state.places = JSON.parse(response.text)
-        this.state.allPlaces = this.state.places
-        this.setState(this.state)
-      })
-  },
-
-  showMarkerDetail(e) {
-    const {target} = e
-    let {place, visitor} = target.dataset
+  showMarkerDetail (event) {
+    let {place, visitor} = event.target.dataset
     const element = document.getElementById('marker-details-modal')
 
     if (place) place = JSON.parse(place)
@@ -61,59 +54,72 @@ const GoogleMap = createClass({
     else element.children[0].innerHTML = 'Unspecified Location'
     element.children[1].innerHTML = message
 
-    const bounds = target.getBoundingClientRect()
+    const bounds = event.target.getBoundingClientRect()
     element.style.display = 'inline-block'
     element.style.left = `${Math.abs(bounds.left + 3)}px`
     element.style.top = `${Math.abs(bounds.top + 3)}px`
   },
 
-  filterPlaces(isVisitor) {
-    const {allPlaces} = this.state
-    const filtered = allPlaces.filter((place) => isVisitor === place.isVisitor)
-
-    this.state.places = filtered
-    this.setState(this.state)
+  getLocations () {
+    this.setState({ error: null })
+    Place.getAll({ condensed: true }, (err, places) => {
+      if (err) return this.setState({ error: err }) // TODO what to do with error?
+      this.state.places.all = this.state.places.current = places
+      this.setState(this.state)
+    })
   },
 
-  myPlaces() {
-    return this.filterPlaces(false)
+  filterPlaces (isVisitor) {
+    const {all} = this.state.places
+    return all.filter((place) => isVisitor === place.isVisitor)
   },
 
-  visitorPlaces() {
-    return this.filterPlaces(true)
+  resetPlaces () {
+    this.state.places.current = this.state.places.all
+    return this.setState(this.state)
   },
 
-  componentWillMount() {
+  selfPlaces () {
+    if (!this.state.places.self.length) {
+      this.state.places.self = this.filterPlaces(false)
+    }
+    this.state.places.current = this.state.places.self
+    return this.setState(this.state)
+  },
+
+  visitorPlaces () {
+    if (!this.state.places.visitors.length) {
+      this.state.places.visitors = this.filterPlaces(true)
+    }
+    this.state.places.current = this.state.places.visitors
+    return this.setState(this.state)
+  },
+
+  componentWillMount () {
     this.getLocations()
   },
 
-  shouldComponentUpdate(nextProps, nextState) {
+  shouldComponentUpdate (nextProps, nextState) {
     return nextState.places === this.state.places
   },
 
-  render() {
+  render () {
+    const {center, zoom} = this.props
     const {places} = this.state
 
     return (
       <DefaultLayout classes="page-map">
         <div id="button-wrap">
-          <div className="link-button dark">
-            <a onClick={this.myPlaces}>Places I've Been &raquo;</a>
-          </div>
-          <div className="link-button dark">
-            <a onClick={this.visitorPlaces}>Visitor Messages &raquo;</a>
-          </div>
-          <div className="link-button dark">
-            <a onClick={this.getLocations}>Show All &raquo;</a>
-          </div>
+          <Button text="Places I've Been" onClick={this.selfPlaces} />
+          <Button text="Visitor Messages" onClick={this.visitorPlaces} />
+          <Button text="Show All" onClick={this.resetPlaces} />
         </div>
         <GoogleMapReact
           options={this.createMapOptions}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}>
-          {places.map((p, i) => {
-            let {location, place, visitor} = p
-
+          defaultCenter={center}
+          defaultZoom={zoom}>
+          {places.current.map((checkin, i) => {
+            const {location, place, visitor} = checkin
             return (
               <Marker
                 key={i}
